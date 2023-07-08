1417a23ab0cfa5a088c92a2e5e2c0e8f89af48f9
İsa <ulutepe42@gmail.com>
2023-07-08 20:32:06 +0300

    Added Player Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private const float MaxX=2f;
+    private const float MinX=-2f;
+}
